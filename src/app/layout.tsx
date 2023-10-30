7318f2814b0cd61ac7b9ae370677da009136889d
Madhur Singh <madhur.singh@sakhaglobal.com>
2023-10-30 10:01:25 +0530

    added common used packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { StoreProvider } from '@/store/store'
+import { Inter } from "next/font/google";
+import "@/scss/globals.scss";
+import { StoreProvider } from "@/store/store";
 
 const inter = Inter({ subsets: ['latin'] })
 
