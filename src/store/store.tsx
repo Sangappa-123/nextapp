80237c2b78f5aa77768448d152fad134825a8f50
Madhur Singh <madhur.singh@sakhaglobal.com>
2023-10-31 15:41:59 +0530

    added root reducer

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -2,17 +2,16 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import React from "react";
+import rootReducer from "@/store/reducers";
 import { Provider } from "react-redux";
-import dashboardSlice from "./reducers/dashboardSlice";
-
-const store = configureStore({reducer: {'dasboard': dashboardSlice}});
+const store = configureStore({ reducer: rootReducer });
 
 export default store;
 
-export const StoreProvider = ({children}:{children:React.ReactNode}) => {
-    return (
-        <Provider store={store}>
-            {children}
-        </Provider>
-    )
+export const StoreProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Provider store={store}>
+      {children}
+    </Provider>
+  )
 }
