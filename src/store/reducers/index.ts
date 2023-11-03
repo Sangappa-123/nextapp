e6feb3964a0befb7ed699cae39c21eb9fb9dd7da
Madhur Singh <madhur.singh@sakhaglobal.com>
2023-11-03 17:29:34 +0530

    removed unused code

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
 import DashboardSlice from "@/store/reducers/Dashboard/DashboardSlice";
-import AppVesionInfoSlice from "./AppVesionInfo/AppVesionInfoSlice";
 
 const rootReducer = combineReducers({
   [DashboardSlice.name]: DashboardSlice.reducer,
-  [AppVesionInfoSlice.name]: AppVesionInfoSlice.reducer,
 });
 
 export default rootReducer;
