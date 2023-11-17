547b99691cc6bb78d5779870ac140018555861d2
Abhishek Srivastava <abhishek.srivastava@sakhaglobal.com>
2023-11-17 11:29:44 +0530

    added login translate

--- src/translations/loginTranslate/en.ts
+++ src/translations/loginTranslate/en.ts
@@ -7,6 +7,11 @@ const translate = {
     passwordPlaceholder: "Password",
     ssoBtn: "Sign in with SSO",
   },
+  inputErrors: {
+    userNameRequired: "User name field is required.",
+    invalidEmail: "Please enter valid email.",
+    passwordRequired: "Password field is required.",
+  },
 };
 
 export { translate };
