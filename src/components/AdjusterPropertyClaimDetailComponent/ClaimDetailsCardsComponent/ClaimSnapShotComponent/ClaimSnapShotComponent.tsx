1b3937cf4d72c0612f0f0944924ac0131ee8d7c1
Madhur Singh <madhur.singh@sakhaglobal.com>
2023-12-11 18:30:29 +0530

    claim detail snap shot card design

--- src/components/AdjusterPropertyClaimDetailComponent/ClaimDetailsCardsComponent/ClaimSnapShotComponent/ClaimSnapShotComponent.tsx
+++ src/components/AdjusterPropertyClaimDetailComponent/ClaimDetailsCardsComponent/ClaimSnapShotComponent/ClaimSnapShotComponent.tsx
@@ -7,33 +7,111 @@ const ClaimSnapShotComponent: React.FC = () => {
     <>
       <Cards className={claimSnapShotStyle.snapShotcardContainer}>
         <GenericComponentHeading title="Claim snapshot" />
-        <div className={claimSnapShotStyle.contentCardsContainer}>
-          <div className="mt-2">
-            <Cards className={claimSnapShotStyle.snapShotContentCard}>
-              <div className={claimSnapShotStyle.cardItemContainer}>
-                <div>Items</div>
-                <div>100 claimed</div>
-                <div>0 processed</div>
-              </div>
-            </Cards>
+        <div className={claimSnapShotStyle.contentContainer}>
+          <div
+            className={`col-md-12 col-sm-12 col-12 ${claimSnapShotStyle.fieldRowContainer}`}
+          >
+            <label className={`col-md-3 col-sm-3 col-6 ${claimSnapShotStyle.fieldLabel}`}>
+              Claim #
+            </label>
+            <div className="col-md-3 col-sm-3 col-6">adjustereStatusWork</div>
+            <label className={`col-md-3 col-sm-3 col-6 ${claimSnapShotStyle.fieldLabel}`}>
+              Status
+            </label>
+            <div className="col-md-3 col-sm-3 col-6">Work In Progress</div>
           </div>
-          <div className="mt-2">
-            <Cards className={claimSnapShotStyle.snapShotContentCard}>
-              <div className={claimSnapShotStyle.cardItemContainer}>
-                <div>Exposure</div>
-                <div>$0.00 Repl</div>
-                <div>$0.00 Cash</div>
-              </div>
-            </Cards>
+          <div
+            className={`col-md-12 col-sm-12 col-12 ${claimSnapShotStyle.fieldRowContainer}`}
+          >
+            <label className={`col-md-3 col-sm-3 col-6 ${claimSnapShotStyle.fieldLabel}`}>
+              Created Date
+            </label>
+            <div className="col-md-3 col-sm-3 col-6">Dec 8, 2023 7:53 PM</div>
+            <label className={`col-md-3 col-sm-3 col-6 ${claimSnapShotStyle.fieldLabel}`}>
+              Elapsed Time
+            </label>
+            <div className="col-md-3 col-sm-3 col-6">2 d 19 h 9 m</div>
           </div>
-          <div className="mt-2">
-            <Cards className={claimSnapShotStyle.snapShotContentCard}>
-              <div className={claimSnapShotStyle.cardItemContainer}>
-                <div>Paid</div>
-                <div>$0.00 Cash</div>
-                <div>$0.00 Holdover</div>
-              </div>
-            </Cards>
+          <div
+            className={`col-md-12 col-sm-12 col-12 ${claimSnapShotStyle.fieldRowContainer}`}
+          >
+            <label className={`col-md-3 col-sm-3 col-6 ${claimSnapShotStyle.fieldLabel}`}>
+              Loss Type
+            </label>
+            <div className="col-md-3 col-sm-3 col-6">Not Specified</div>
+            <label
+              className={`col-md-3 col-sm-3 col-6 ps-1 ${claimSnapShotStyle.fieldLabel}`}
+            >
+              Claim Deductible
+            </label>
+            <div className="col-md-3 col-sm-3 col-6">$11.00</div>
+          </div>
+          <div
+            className={`col-md-12 col-sm-12 col-12 ${claimSnapShotStyle.fieldRowContainer}`}
+          >
+            <label className={`col-md-3 col-sm-3 col-6 ${claimSnapShotStyle.fieldLabel}`}>
+              Tax %
+            </label>
+            <div className="col-md-3 col-sm-3 col-6">11</div>
+            <label className={`col-md-3 col-sm-3 col-6 ${claimSnapShotStyle.fieldLabel}`}>
+              Content Limits
+            </label>
+            <div className="col-md-3 col-sm-3 col-6">$11.00</div>
+          </div>
+          <div
+            className={`col-md-12 col-sm-12 col-12 ${claimSnapShotStyle.fieldRowContainer}`}
+          >
+            <label className={`col-md-3 col-sm-3 col-6 ${claimSnapShotStyle.fieldLabel}`}>
+              Min. $ Item to Price
+            </label>
+            <div className="col-md-3 col-sm-3 col-6">$1.00</div>
+          </div>
+          <div className={claimSnapShotStyle.contentCardsContainer}>
+            <div className="mt-2">
+              <Cards className={claimSnapShotStyle.snapShotContentCard}>
+                <div className={claimSnapShotStyle.cardItemContainer}>
+                  <div className={claimSnapShotStyle.itemTitle}>Items</div>
+                  <div>
+                    <span className={claimSnapShotStyle.numericContent}>100</span>
+                    <span className={claimSnapShotStyle.textContent}>claimed</span>
+                  </div>
+                  <div>
+                    <span className={claimSnapShotStyle.numericContent}>0</span>
+                    <span className={claimSnapShotStyle.textContent}>processed</span>
+                  </div>
+                </div>
+              </Cards>
+            </div>
+            <div className="mt-2">
+              <Cards className={claimSnapShotStyle.snapShotContentCard}>
+                <div className={claimSnapShotStyle.cardItemContainer}>
+                  <div className={claimSnapShotStyle.itemTitle}>Exposure</div>
+                  <div>
+                    <span className={claimSnapShotStyle.numericContent}>$0.00</span>
+                    <span className={claimSnapShotStyle.textContent}>Repl</span>
+                  </div>
+                  <div>
+                    <span className={claimSnapShotStyle.numericContent}>$0.00</span>
+                    <span className={claimSnapShotStyle.textContent}>Cash</span>
+                  </div>
+                </div>
+              </Cards>
+            </div>
+            <div className="mt-2">
+              <Cards className={claimSnapShotStyle.snapShotContentCard}>
+                <div className={claimSnapShotStyle.cardItemContainer}>
+                  <div className={claimSnapShotStyle.itemTitle}>Paid</div>
+                  <div>
+                    <span className={claimSnapShotStyle.numericContent}>$0.00</span>
+                    <span className={claimSnapShotStyle.textContent}>Cash</span>
+                  </div>
+                  <div>
+                    <span className={claimSnapShotStyle.numericContent}>$0.00</span>
+                    <span className={claimSnapShotStyle.textContent}>Holdover</span>
+                  </div>
+                </div>
+              </Cards>
+            </div>
           </div>
         </div>
       </Cards>
