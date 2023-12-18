15615098e98401d874b87414755ee995b478294e
Madhur Singh <madhur.singh@sakhaglobal.com>
2023-12-18 13:45:06 +0530

    refector api call on calimdetail page

--- src/container/AdjusterPropertyClaimDetailContainer/AdjusterPropertyClaimDetailContainer.tsx
+++ src/container/AdjusterPropertyClaimDetailContainer/AdjusterPropertyClaimDetailContainer.tsx
@@ -1,10 +1,25 @@
 import AdjusterPropertyClaimDetailComponent from "@/components/AdjusterPropertyClaimDetailComponent";
-
-function AdjusterPropertyClaimDetailContainer({ claimId }: { claimId: string }) {
+import { claimContentList } from "@/services/ClaimContentListService";
+import { serviceRequestList } from "@/services/ClaimServiceRequestListService";
+interface propsTypes {
+  claimId: string;
+}
+const AdjusterPropertyClaimDetailContainer: React.FC<propsTypes> = async ({
+  claimId,
+}) => {
+  const payload = {
+    claimId: claimId,
+  };
+  const claimContentListRes: any = await claimContentList(payload);
+  const serviceRequestListRes: any = await serviceRequestList(payload);
   return (
     <>
-      <AdjusterPropertyClaimDetailComponent claimId={claimId} />
+      <AdjusterPropertyClaimDetailComponent
+        claimId={claimId}
+        claimContentListRes={claimContentListRes}
+        serviceRequestListRes={serviceRequestListRes}
+      />
     </>
   );
-}
+};
 export default AdjusterPropertyClaimDetailContainer;
--- src/services/ClaimContentListService.ts
+++ src/services/ClaimContentListService.ts
@@ -1,29 +1,18 @@
-import { getHeaderWithoutToken } from "@/utils/HeaderService";
 import { getApiEndPoint } from "./ApiEndPointConfig";
 import store from "@/store/store";
 import { addClaimContentListData } from "@/reducers/ClaimData/ClaimContentSlice";
-
-import { getClientCookie } from "@/utils/utitlity";
 import HttpService from "@/HttpService";
 
-interface objectType {
-  [key: string | number]: any;
-}
-export const claimContentList = async (payload: any, token: any): Promise<objectType> => {
-  const headersData: object = getHeaderWithoutToken();
-
-  return new Promise((resolve, rejects) => {
-    fetch(getApiEndPoint("claimContentList"), {
-      method: "POST",
-      headers: { ...headersData, "X-Auth-Token": token },
-      body: JSON.stringify(payload),
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        return resolve({ result });
-      })
-      .catch((error) => rejects({ error }));
-  });
+export const claimContentList = async (payload: { claimId: string | any }) => {
+  try {
+    const url = getApiEndPoint("claimContentList");
+    const http = new HttpService();
+    const res = await http.post(url, payload);
+    return res;
+  } catch (error) {
+    console.warn("claimContentList__err", error);
+    throw error;
+  }
 };
 
 export const claimContentListV2 = async (payload: { claimId: string }) => {
@@ -33,7 +22,7 @@ export const claimContentListV2 = async (payload: { claimId: string }) => {
     const res = await http.post(url, payload);
     return res;
   } catch (error) {
-    console.warn("claimContentListV2__err", error);
+    console.error("claimContentListV2__err", error);
     throw error;
   }
 };
@@ -41,15 +30,10 @@ export const claimContentListV2 = async (payload: { claimId: string }) => {
 export const fetchClaimContentList = async () => {
   const state = store.getState();
   const claimId = state.claimdata.claimId;
-
-  const token = await getClientCookie("accessToken");
-
   const payload = {
     claimId,
   };
-
-  const claimcontentListRes: any = await claimContentList(payload, token);
-
+  const claimcontentListRes: any = await claimContentList(payload);
   if (claimcontentListRes.result.status === 200) {
     const claimContentData = claimcontentListRes.result;
     store.dispatch(
