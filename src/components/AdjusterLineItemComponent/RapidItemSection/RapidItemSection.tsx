275e685b243ea020418761d956728308437c9372
Abhishek Srivastava <abhishek.srivastava@sakhaglobal.com>
2024-01-08 18:31:51 +0530

    synced rapid section and original item category and sub-category

--- src/components/AdjusterLineItemComponent/RapidItemSection/RapidItemSection.tsx
+++ src/components/AdjusterLineItemComponent/RapidItemSection/RapidItemSection.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import rapidItemSectionStyle from "./rapidItemSection.module.scss";
 import OriginalItemRapidSection from "./OriginalItemRapidSection";
 import ReplacementItemRapidSection from "./ReplacementItemRapidSection";
+import { OriginalItemRefType } from "../LineItemDetailComponent/OrginalItemForm/OrginalItemForm";
 
-function RapidItemSection() {
+interface propType {
+  originalItemRef: OriginalItemRefType | null;
+}
+function RapidItemSection(props: propType) {
   return (
     <div className={rapidItemSectionStyle.root}>
-      <OriginalItemRapidSection />
+      <OriginalItemRapidSection originalItemRef={props.originalItemRef} />
       <ReplacementItemRapidSection />
     </div>
   );
