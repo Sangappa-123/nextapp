82d60de1d80a55afec1eff7cdd0bbfcb0883672c
Madhur Singh <madhur.singh@sakhaglobal.com>
2024-01-16 19:41:04 +0530

    resolved conflicts

--- src/components/AdjusterPropertyClaimDetailComponent/ClaimDetailsCardsComponent/ClaimSnapShotComponent/RightActionsButton/RightActionsButton.tsx
+++ src/components/AdjusterPropertyClaimDetailComponent/ClaimDetailsCardsComponent/ClaimSnapShotComponent/RightActionsButton/RightActionsButton.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import actionsBtnStyle from "./RightActionsButton.module.scss";
 import { claimDetailsTranslateType } from "@/translations/claimDetailsTranslate/en";
 import useTranslation from "@/hooks/useTranslation";
+import GenericButton from "@/components/common/GenericButton";
+
 type actionsType = {
   setShowForm: any;
 };
@@ -14,30 +16,39 @@ const RightActionsComponent: React.FC<actionsType> = (props: any) => {
   return (
     <div className={actionsBtnStyle.actionBtnContainer}>
       {!showActionBtn && (
-        <div
-          className={actionsBtnStyle.editActionBtn}
-          onClick={() => {
-            props.setShowForm(true);
-            setShowActionBtn(true);
-          }}
-        >
-          {translate?.claimSnapshot?.edit}
+        <div className={actionsBtnStyle.actionBtns}>
+          <GenericButton
+            size="small"
+            label={translate?.claimSnapshot?.edit}
+            theme="linkBtn"
+            btnClassname={actionsBtnStyle.linkBtnStyle}
+            onClick={() => {
+              props.setShowForm(true);
+              setShowActionBtn(true);
+            }}
+          />
         </div>
       )}
       {showActionBtn && (
         <div className={actionsBtnStyle.actionBtns}>
-          <span className={actionsBtnStyle.updateActionBtn}>
-            {translate?.claimSnapshot?.update}
-          </span>
-          <span
-            className={actionsBtnStyle.cancelActionBtn}
+          <GenericButton
+            type="submit"
+            size="small"
+            label={translate?.claimSnapshot?.update}
+            btnClassname={actionsBtnStyle.linkBtnStyle}
+            theme="linkBtn"
+            form="claim-info-update-form"
+          />
+          <GenericButton
+            size="small"
+            label={translate?.claimSnapshot?.cancel}
+            theme="linkBtn"
+            btnClassname={actionsBtnStyle.linkBtnStyle}
             onClick={() => {
               props.setShowForm(false);
               setShowActionBtn(false);
             }}
-          >
-            {translate?.claimSnapshot?.cancel}
-          </span>
+          />
         </div>
       )}
     </div>
--- src/components/AdjusterPropertyClaimDetailComponent/ClaimDetailsCardsComponent/ClaimSnapShotComponent/UpdateClaimInfoForm/UpdateClaimInfoForm.tsx
+++ src/components/AdjusterPropertyClaimDetailComponent/ClaimDetailsCardsComponent/ClaimSnapShotComponent/UpdateClaimInfoForm/UpdateClaimInfoForm.tsx
@@ -4,237 +4,264 @@ import GenericInput from "@/components/common/GenericInput";
 import { useForm, Controller } from "react-hook-form";
 import GenericSelect from "@/components/common/GenericSelect";
 import { convertToCurrentTimezone } from "@/utils/helper";
+import { getLossTypes } from "@/services/AdjusterPropertyClaimDetailServices/ClaimSnapShotService";
+import { useEffect, useMemo, useState } from "react";
+import Loading from "@/app/[lang]/loading";
 interface UpdateClaimInfoType {
   claimSnapShotData: any;
   translate: any;
+  defaultValue: object;
 }
 
 const UpdateClaimInfoForm: React.FC<UpdateClaimInfoType> = ({
   claimSnapShotData,
   translate,
+  defaultValue,
 }) => {
+  const [selectOptions, setSelectOptions] = useState([]);
+  const [loading, setLoading] = useState(false);
   const dateFormate = "MMM DD, YYYY h:mm A";
-
-  const selectOptions = [
-    {
-      id: 1,
-      name: "Water",
-      active: true,
-    },
-    {
-      id: 2,
-      name: "Fire/Smoke",
-      active: true,
-    },
-    {
-      id: 3,
-      name: "Lightning",
-      active: true,
-    },
-    {
-      id: 4,
-      name: "Theft From Vehicle",
-      active: true,
-    },
-    {
-      id: 5,
-      name: "Theft From Home",
-      active: true,
-    },
-    {
-      id: 6,
-      name: "Mysterious Disappearance",
-      active: true,
-    },
-    {
-      id: 7,
-      name: "Vandalism",
-      active: true,
-    },
-    {
-      id: 8,
-      name: "Wind/Tornado/Hurricane/Hail",
-      active: true,
-    },
-    {
-      id: 9,
-      name: "Not Specified",
-      active: true,
-    },
-  ];
+  useEffect(() => {
+    const fetchLossType = async () => {
+      setLoading(true);
+      const lossTypeRes = await getLossTypes();
+      setSelectOptions(lossTypeRes?.data);
+      setLoading(false);
+    };
+    fetchLossType();
+  }, []);
 
   const selectBoxStyles = {
-    control: (styles: any) => ({
-      ...styles,
-      backgroundColor: "white",
-      border: "1px solid #c2cad8",
+    control: {
       boxShadow: "none",
       "&:focus, &:active": {
-        border: "1px solid #4169e1",
+        border: "none",
       },
-      height: "22px",
-      minHeight: "22px",
-    }),
-    valueContainer: (styles: any) => ({ ...styles, bottom: "1.3px" }),
-    dropdownIndicator: (styles: any) => ({
-      ...styles,
-      padding: "1px",
-      height: "22px",
-      width: "15px",
-    }),
-    clearIndicator: (styles: any) => ({
-      ...styles,
-      padding: "1px",
-      height: "22px",
-      width: "15px",
-    }),
+      border: "none",
+      height: "16.67px",
+      minHeight: "16.67px",
+      bottom: "1.5px",
+      paddingLeft: 0,
+      paddingTop: 0,
+    },
+    singleValue: {
+      fontSize: "12px",
+      marginLeft: 0,
+    },
+    dropdownIndicator: {
+      paddingTop: 0,
+      paddingBottom: 0,
+    },
+    indicatorSeparator: {
+      display: "none",
+    },
+    clearIndicator: {
+      display: "none",
+    },
   };
+
+  // console.log("claimSnapShotData", claimSnapShotData);
 
   const defaultValues = {
     claimId: claimSnapShotData?.claimId,
     updatedClaimNumber: claimSnapShotData?.claimNumber,
     oldClaimNumber: claimSnapShotData?.claimNumber,
-    damageTypeId: claimSnapShotData?.damageType,
+    damageTypeId: claimSnapShotData?.damageTypeId,
+    damageType: claimSnapShotData?.damageType,
     taxRate: claimSnapShotData?.taxRate,
     deductible: claimSnapShotData?.deductible,
     minimumThreshold: claimSnapShotData?.minimumThreshold,
-    totalPolicyCoverage: 10000,
+    totalPolicyCoverage: claimSnapShotData?.policyLimit,
     policyLimit: claimSnapShotData?.policyLimit,
-    individualLimit: null,
+    individualLimit: claimSnapShotData?.individualLimit,
     isUpdatedByInsuranceUser: true,
     shippingDate: null,
     shippingMethod: null,
-    additionalNote: null,
+    additionalNote: claimSnapShotData?.additionalNote,
   };
 
-  const { register, handleSubmit, setValue, control } = useForm({ defaultValues });
+  const { register, handleSubmit, control, setValue } = useForm({ defaultValues });
+
+  useMemo(() => {
+    if (defaultValue) setValue("damageTypeId", defaultValue);
+  }, [defaultValue, setValue]);
 
   const submitHandler = async (data: any) => {
-    console.log("Submit data", data);
+    const payload = {
+      ...data,
+      damageTypeId: data?.damageTypeId?.id,
+      claimProfile: "Contents",
+    };
+    console.log("Submit data", payload);
   };
 
   return (
-    <form id="claim-snapshot-form" onSubmit={handleSubmit(submitHandler)}>
-      <div className={`col-md-12 col-sm-12 col-12 ${ClmainInfoStyle.fieldRowContainer}`}>
-        <fieldset
-          className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
+    <>
+      {loading && <Loading />}
+      <form id="claim-info-update-form" onSubmit={handleSubmit(submitHandler)}>
+        <div
+          className={`col-md-12 col-sm-12 col-12 ${ClmainInfoStyle.fieldRowContainer}`}
         >
-          <legend className={ClmainInfoStyle.fieldSetLabel}>
-            {translate?.claimSnapshot?.claim}
-          </legend>
-          <GenericInput
-            setValue={setValue}
-            {...register("updatedClaimNumber")}
-            inputFieldClassname={ClmainInfoStyle.customInput}
-          />
-        </fieldset>
-        <fieldset
-          className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
+          <fieldset
+            className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
+          >
+            <legend className={ClmainInfoStyle.fieldSetLabel}>
+              {translate?.claimSnapshot?.claim}
+            </legend>
+            <GenericInput
+              {...register("updatedClaimNumber")}
+              inputFieldClassname={ClmainInfoStyle.customInput}
+            />
+          </fieldset>
+          <fieldset
+            className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
+          >
+            <legend className={ClmainInfoStyle.fieldSetLabel}>
+              {translate?.claimSnapshot?.status}
+            </legend>
+            <div className={ClmainInfoStyle.fieldValue}>
+              {claimSnapShotData?.claimStatus?.status}
+            </div>
+          </fieldset>
+          <fieldset
+            className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
+          >
+            <legend className={ClmainInfoStyle.fieldSetLabel}>
+              {translate?.claimSnapshot?.tax}
+            </legend>
+            <Controller
+              name="taxRate"
+              control={control}
+              render={({ field }: any) => (
+                <GenericInput
+                  inputFieldClassname={ClmainInfoStyle.customInput}
+                  value={defaultValues?.policyLimit}
+                  onValueChange={(values: any) => field.onChange(values.value)}
+                  percentageFormatter={true}
+                  type="number"
+                  {...field}
+                />
+              )}
+            />
+          </fieldset>
+        </div>
+        <div
+          className={`col-md-12 col-sm-12 col-12 ${ClmainInfoStyle.fieldRowContainer}`}
         >
-          <legend className={ClmainInfoStyle.fieldSetLabel}>
-            {translate?.claimSnapshot?.status}
-          </legend>
-          <div className={ClmainInfoStyle.fieldValue}>
-            {claimSnapShotData?.claimStatus?.status}
-          </div>
-        </fieldset>
-        <fieldset
-          className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
+          <fieldset
+            className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
+          >
+            <legend className={ClmainInfoStyle.fieldSetLabel}>
+              {translate?.claimSnapshot?.elapsedTime}
+            </legend>
+            <div className={ClmainInfoStyle.fieldValue}>
+              {claimSnapShotData?.claimTime}
+            </div>
+          </fieldset>
+          <fieldset
+            className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
+          >
+            <legend className={ClmainInfoStyle.fieldSetLabel}>
+              {translate?.claimSnapshot?.coverageLimits}
+            </legend>
+            <Controller
+              name="policyLimit"
+              control={control}
+              render={({ field }: any) => (
+                <GenericInput
+                  {...register("policyLimit")}
+                  inputFieldClassname={ClmainInfoStyle.customInput}
+                  value={defaultValues?.policyLimit}
+                  // type="number"
+                  onValueChange={(values: any) => field.onChange(values.value)}
+                  priceFormatter={true}
+                  {...field}
+                />
+              )}
+            />
+          </fieldset>
+          <fieldset
+            className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
+          >
+            <legend className={ClmainInfoStyle.fieldSetLabel}>
+              {translate?.claimSnapshot?.deductible}
+            </legend>
+            <Controller
+              name="deductible"
+              control={control}
+              render={({ field }: any) => (
+                <GenericInput
+                  inputFieldClassname={ClmainInfoStyle.customInput}
+                  value={defaultValues?.deductible}
+                  onValueChange={(values: any) => field.onChange(values.value)}
+                  priceFormatter={true}
+                  {...field}
+                />
+              )}
+            />
+          </fieldset>
+        </div>
+        <div
+          className={`col-md-12 col-sm-12 col-12 ${ClmainInfoStyle.fieldRowContainer}`}
         >
-          <legend className={ClmainInfoStyle.fieldSetLabel}>
-            {translate?.claimSnapshot?.tax}
-          </legend>
-          <GenericInput
-            setValue={setValue}
-            {...register("taxRate")}
-            inputFieldClassname={ClmainInfoStyle.customInput}
-            type="number"
-          />
-        </fieldset>
-      </div>
-      <div className={`col-md-12 col-sm-12 col-12 ${ClmainInfoStyle.fieldRowContainer}`}>
-        <fieldset
-          className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
-        >
-          <legend className={ClmainInfoStyle.fieldSetLabel}>
-            {translate?.claimSnapshot?.elapsedTime}
-          </legend>
-          <div className={ClmainInfoStyle.fieldValue}>{claimSnapShotData?.claimTime}</div>
-        </fieldset>
-        <fieldset
-          className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
-        >
-          <legend className={ClmainInfoStyle.fieldSetLabel}>
-            {translate?.claimSnapshot?.contentLimits}
-          </legend>
-          <GenericInput
-            setValue={setValue}
-            {...register("policyLimit")}
-            inputFieldClassname={ClmainInfoStyle.customInput}
-            type="number"
-          />
-        </fieldset>
-        <fieldset
-          className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
-        >
-          <legend className={ClmainInfoStyle.fieldSetLabel}>
-            {translate?.claimSnapshot?.claimDeductible}
-          </legend>
-          <GenericInput
-            {...register("deductible")}
-            inputFieldClassname={ClmainInfoStyle.customInput}
-            type="number"
-          />
-        </fieldset>
-      </div>
-      <div className={`col-md-12 col-sm-12 col-12 ${ClmainInfoStyle.fieldRowContainer}`}>
-        <fieldset
-          className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
-        >
-          <legend className={ClmainInfoStyle.fieldSetLabel}>
-            {translate?.claimSnapshot?.createdDate}
-          </legend>
-          <div className={ClmainInfoStyle.fieldValue}>
-            {convertToCurrentTimezone(claimSnapShotData?.createdDate, dateFormate)}
-          </div>
-        </fieldset>
-        <fieldset
-          className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
-        >
-          <legend className={ClmainInfoStyle.fieldSetLabel}>
-            {translate?.claimSnapshot?.minItemToPrice}
-          </legend>
-          <GenericInput
-            {...register("minimumThreshold")}
-            inputFieldClassname={ClmainInfoStyle.customInput}
-            type="number"
-          />
-        </fieldset>
-        <fieldset
-          className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
-        >
-          <legend className={ClmainInfoStyle.fieldSetLabel}>
-            {translate?.claimSnapshot?.lossType}
-          </legend>
-          <Controller
-            control={control}
-            name={"damageTypeId"}
-            rules={{ required: true }}
-            render={({ field: { ...rest } }: any) => (
-              <GenericSelect
-                customStyles={selectBoxStyles}
-                isSearchable={false}
-                options={selectOptions}
-                getOptionLabel={(option: { name: any }) => option.name}
-                getOptionValue={(option: { id: any }) => option.id}
-                name={"selectOptions"}
-                {...rest}
-              />
-            )}
-          />
-        </fieldset>
-      </div>
-    </form>
+          <fieldset
+            className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
+          >
+            <legend className={ClmainInfoStyle.fieldSetLabel}>
+              {translate?.claimSnapshot?.createdDate}
+            </legend>
+            <div className={ClmainInfoStyle.fieldValue}>
+              {convertToCurrentTimezone(claimSnapShotData?.createdDate, dateFormate)}
+            </div>
+          </fieldset>
+          <fieldset
+            className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
+          >
+            <legend className={ClmainInfoStyle.fieldSetLabel}>
+              {translate?.claimSnapshot?.minItemToPrice}
+            </legend>
+            <Controller
+              name="minimumThreshold"
+              control={control}
+              render={({ field }: any) => (
+                <GenericInput
+                  inputFieldClassname={ClmainInfoStyle.customInput}
+                  value={defaultValues?.minimumThreshold}
+                  onValueChange={(values: any) => field.onChange(values.value)}
+                  priceFormatter={true}
+                  {...field}
+                />
+              )}
+            />
+          </fieldset>
+          <fieldset
+            className={`col-md-3 col-sm-3 col-6 ${ClmainInfoStyle.fieldSetContainer}`}
+          >
+            <legend className={ClmainInfoStyle.fieldSetLabel}>
+              {translate?.claimSnapshot?.lossType}
+            </legend>
+            <Controller
+              control={control}
+              name={"damageTypeId"}
+              rules={{ required: true }}
+              defaultValue={defaultValue}
+              render={({ field: { ...rest } }: any) => (
+                <GenericSelect
+                  customStyles={selectBoxStyles}
+                  isClearable={false}
+                  isSearchable={false}
+                  options={selectOptions}
+                  getOptionLabel={(option: { name: any }) => option.name}
+                  getOptionValue={(option: { id: any }) => option.id}
+                  name={"damageTypeId"}
+                  {...rest}
+                />
+              )}
+            />
+          </fieldset>
+        </div>
+      </form>
+    </>
   );
 };
 export default UpdateClaimInfoForm;
